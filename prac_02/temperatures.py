84414cb8b9fbfe7981e290de9d491e8e4a23544d
raaro1 <roi.aaron@my.jcu.edu.au>
2025-09-28 01:50:38 +1000

    Add docstrings and fix refactoring

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -3,34 +3,38 @@ CP1404/CP5632 - Practical
 Program for temperature conversion
 """
 
-MENU = """C - Convert Celsius to Fahrenheit
+
+def main():
+    """Main Menu"""
+    MENU = """C - Convert Celsius to Fahrenheit
 F - Convert Fahrenheit to Celsius
 Q - Quit"""
-print(MENU)
-choice = input(">>> ").upper()
+    print(MENU)
+    choice = input(">>> ").upper()
+    while choice != "Q":
+        if choice == "C":
+            convert_celsius()
+        elif choice == "F":
+            convert_fahrenheit()
+        else:
+            print("Invalid option")
+        print(MENU)
+        choice = input(">>> ").upper()
+    print("Thank you.")
 
 
 def convert_celsius():
-    global celsius, fahrenheit
+    """Convert Celsius to Fahrenheit"""
     celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
     print(f"Result: {fahrenheit:.2f} F")
 
 
 def convert_fahrenheit():
-    global fahrenheit, celsius
+    """Convert Fahrenheit to Celsius"""
     fahrenheit = float(input("Fahrenheit : "))
     celsius = 5 / 9 * (fahrenheit - 32)
     print(f"Result: {celsius:.2f} C")
 
 
-while choice != "Q":
-    if choice == "C":
-        convert_celsius()
-    elif choice == "F":
-        convert_fahrenheit()
-    else:
-        print("Invalid option")
-    print(MENU)
-    choice = input(">>> ").upper()
-print("Thank you.")
+main()
