20fb9e4feebec5ae3482a801dcb2135df7da615d
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-04 13:53:45 +1000

    add numbers.txt

---
+++ prac_03/files.py
@@ -0,0 +1,13 @@
+# # 1.
+# out_file = open("name.txt", "w")
+# name = input("Enter your name: ")
+# print(name, file=out_file)
+# out_file.close()
+
+# 2.
+in_file = open("name.txt", "r")
+line = in_file.readline()
+print(line)
+in_file.close()
+
+# 3.
