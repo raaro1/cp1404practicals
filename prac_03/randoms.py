5160978639cd4bf4587a37fb1ceb5af29082e223
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-04 11:03:30 +1000

    run random.randint test in python console

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,9 +1,3 @@
-import random
-
-print(random.randint(5, 20))  # line 1
-print(random.randrange(3, 10, 2))  # line 2
-print(random.uniform(2.5, 5.5))  # line 3
-
 # What did you see on line 1?
 
 # What was the smallest number you could have seen, what was the largest?
