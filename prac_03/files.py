6ac723aa463f0fa92a589d74b4c205bb796bfc3d
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-04 14:13:22 +1000

    finish questions

--- prac_03/files.py
+++ prac_03/files.py
@@ -5,9 +5,24 @@
 # out_file.close()
 
 # 2.
-in_file = open("name.txt", "r")
-line = in_file.readline()
-print(line)
-in_file.close()
+# in_file = open("name.txt", "r")
+# line = in_file.read()
+# print(line)
+# in_file.close()
 
 # 3.
+# with open("numbers.txt", "r") as out_file:
+#     line_one = out_file.readline()
+#     line_two = out_file.readline()
+#     total = int(line_one) + int(line_two)
+#     print(f"{total}")
+
+# # 4.
+# with open('numbers.txt', 'r') as out_file:
+#     total = 0
+#     for line in out_file:
+#         total += int(line)
+#
+#     print(total)
+
+
