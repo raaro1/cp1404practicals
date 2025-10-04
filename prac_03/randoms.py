29ab943b41f2533785cc71a79af26f05c947a22e
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-04 11:21:17 +1000

    finish questions

--- prac_03/randoms.py
+++ prac_03/randoms.py
@@ -1,10 +1,20 @@
 # What did you see on line 1?
+# a random integer between 5 and 20 inclusive
 
 # What was the smallest number you could have seen, what was the largest?
-#
+# the smallest number possible would be 5, and the largest number would be 20
+
 # What did you see on line 2?
+# a random integer between 3 and 10 that steps/increments 2 times
+
 # What was the smallest number you could have seen, what was the largest?
+# 3 and 9 would be the smallest and largest numbers possible as it increments by 2, so 10 wouldn't be possible
+
 # Could line 2 have produced a 4?
-#
+# no, as the only possible numbers would be 3,5,7,9
+
 # What did you see on line 3?
+# random floating decimals between 2.5 and 5.5 inclusive being printed
+
 # What was the smallest number you could have seen, what was the largest?
+# the smallest number would be 2.5 and the 5.5 that could be possibly seen
