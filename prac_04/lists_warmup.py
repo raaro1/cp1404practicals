1d2bc66994b9072e4b01282ee29264a0fe26efeb
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-12 02:27:50 +1000

    finish warmup

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,25 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# 3
+# 2
+# 1
+# 3,1,4,1,5,9
+# 1
+# true
+# false
+# false
+# 3,1,4,1,5,9,2,6,5,3
+
+# 1.
+numbers[0] = "ten"
+
+# 2.
+numbers[-1] = 1
+
+# 3.
+print(numbers[2:])
+
+# 4.
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(9 in numbers)
+
