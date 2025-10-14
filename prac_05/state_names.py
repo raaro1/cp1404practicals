700ddddb3a5f320cbbcb66a0800c4565f03a37b8
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-14 13:44:17 +1000

    Add upper() string method

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -12,6 +12,7 @@ state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
+
     else:
         print("Invalid short state")
     state_code = input("Enter short state: ").upper()
