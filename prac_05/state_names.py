faf6949a452233275e4d6637c2fd0a2fcceebd77
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-19 20:37:28 +1000

    Added exception to state code

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -10,10 +10,14 @@ print(CODE_TO_NAME)
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
-    if state_code in CODE_TO_NAME:
+    # if state_code in CODE_TO_NAME:
+    try:
         print(state_code, "is", CODE_TO_NAME[state_code])
+        for state_abbreviation, full_name  in CODE_TO_NAME.items():
+            print(f"{state_abbreviation} is {full_name}.")
+        state_code = input("Enter short state: ").upper()
+    except KeyError:
+        print(f"{state_code} is not a valid state.")
+        state_code = input("Enter short state: ").upper()
 
-    else:
-        print("Invalid short state")
-    state_code = input("Enter short state: ").upper()
 
