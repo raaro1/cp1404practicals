fb33184f1260be6c70629482b78e9b5f9de32875
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-19 20:49:08 +1000

    Create dictionary for color codes

---
+++ prac_05/hex_colours.py
@@ -0,0 +1,12 @@
+COLOR_CODES = {
+    "Absolute Zero": "#0048ba",
+    "Acid Green": "#b0bf1a",
+    "AliceBlue": "#f0f8ff",
+    "Alizarin crimson": "#e32636",
+    "Amaranth": "#e52b50",
+    "Amber": "#ffbf00",
+    "Amethyst": "#9966cc",
+    "AntiqueWhite": "#faebd7",
+    "Apricot": "#fbceb1",
+    "Aqua": "#00ffff"
+}
