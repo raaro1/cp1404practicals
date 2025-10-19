8fc732002205dd1f720efed0d8ae83f8cfad85aa
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-19 22:47:23 +1000

    Fix split and user input logic, as well as .join

---
+++ prac_05/emails.py
@@ -0,0 +1,28 @@
+"""
+emails
+Estimate: 40 minutes
+Actual:     minutes
+"""
+
+EMAIL_TO_NAME = {}
+
+email = input("Email: ")
+while email != "":
+    email_address = email
+    username = email.split("@")[0]
+    name_parts = username.split(".")
+    name = " ".join(part.title() for part in name_parts)
+
+    user_confirms_name = input(f"Is your name {name}? (Y/n) ").lower()
+    if user_confirms_name in ("y",""):
+        EMAIL_TO_NAME[email] = name
+    else:
+        name = input("What is your name? ").title()
+        EMAIL_TO_NAME[email] = name
+
+    email = input("Email: ")
+
+print()
+
+for email, name in EMAIL_TO_NAME.items():
+    print(f"{name}: {email}")
