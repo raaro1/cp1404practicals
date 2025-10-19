f488cc4d9851f9ff04302358b32dde8d975b3190
raaro1 <roi.aaron@my.jcu.edu.au>
2025-10-19 21:07:24 +1000

    Add exceptions to colour code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,12 +1,21 @@
-COLOR_TO_CODES = {
-    "Absolute Zero": "#0048ba",
-    "Acid Green": "#b0bf1a",
-    "AliceBlue": "#f0f8ff",
-    "Alizarin crimson": "#e32636",
-    "Amaranth": "#e52b50",
-    "Amber": "#ffbf00",
-    "Amethyst": "#9966cc",
-    "AntiqueWhite": "#faebd7",
-    "Apricot": "#fbceb1",
-    "Aqua": "#00ffff"
+COLOUR_TO_CODES = {
+    "absolutezero": "#0048ba",
+    "acidgreen": "#b0bf1a",
+    "aliceblue": "#f0f8ff",
+    "alizarincrimson": "#e32636",
+    "amaranth": "#e52b50",
+    "amber": "#ffbf00",
+    "amethyst": "#9966cc",
+    "antiquewhite": "#faebd7",
+    "apricot": "#fbceb1",
+    "aqua": "#00ffff"
 }
+print(COLOUR_TO_CODES)
+colour_name = input("Enter a colour: ").lower()
+while colour_name != "":
+    try:
+        print(colour_name, "is", COLOUR_TO_CODES[colour_name])
+        colour_name = input("Enter a colour: ").lower()
+    except KeyError:
+        print(f"{colour_name} is not a valid colour.")
+        colour_name = input("Enter a colour: ").lower()
