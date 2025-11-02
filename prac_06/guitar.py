7b9f78226a1fa5414c6b96fbab173ec284750ae4
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-02 20:34:41 +1000

    Add docstrings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,15 +4,18 @@ Actual:
 """
 
 class Guitar:
+    """Adds attributes to newly created objects"""
     def __init__(self, name="", year=0, cost=0):
         self.name = name
         self.year = year
         self.cost = cost
 
     def get_age(self):
+        """Returns the age of the guitar"""
         return self.year - self.year
 
     def is_vintage(self):
+        """Checks if object is vintage"""
         return self.get_age() > 50
 
     def __str__(self):
