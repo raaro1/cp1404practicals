f673ffe3c0c479fa892b5fa788d4e6c721fc4f3b
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-02 20:13:11 +1000

    Add docstring to programming_language.py

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,9 +1,10 @@
 """
 Estimate: 15 minutes
-Actual:
+Actual: 18 minutes
 """
 
 class ProgrammingLanguage:
+    """Gives attributes about the programming language"""
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -11,8 +12,10 @@ class ProgrammingLanguage:
         self.year = year
 
     def is_dynamic(self):
+        """Checks if the object is dynamic"""
         return self.typing == "Dynamic"
 
 
     def __str__(self):
+        """Returns a string representation of the object"""
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year})"
