fb535d3c375e558676b283d7ebe29a1d2d3e6c30
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-02 20:56:55 +1000

    Fix get_age by adding constants

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -3,6 +3,9 @@ Estimated: 30 minutes
 Actual:
 """
 
+CURRENT_YEAR = 2025
+VINTAGE_AGE = 50
+
 class Guitar:
     """Adds attributes to newly created objects"""
     def __init__(self, name="", year=0, cost=0):
@@ -12,11 +15,11 @@ class Guitar:
 
     def get_age(self):
         """Returns the age of the guitar"""
-        return self.year - self.year
+        return CURRENT_YEAR - self.year
 
     def is_vintage(self):
         """Checks if object is vintage"""
-        return self.get_age() > 50
+        return self.get_age() > VINTAGE_AGE
 
     def __str__(self):
         """Returns a string representation of the object"""
