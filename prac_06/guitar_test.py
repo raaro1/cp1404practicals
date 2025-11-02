238d6c8e7d8a781f4e1ee73de2ad9f814eb23439
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-02 20:57:09 +1000

    Add proper calling of functions

--- prac_06/guitar_test.py
+++ prac_06/guitar_test.py
@@ -5,9 +5,9 @@ def run_test():
     gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
     other = Guitar("Another Guitar", 2013, 1456.9)
 
-    print(f"{gibson.name} get_age() - Expected 103. Got {gibson.get_age}")
-    print(f"Other {other.name} get_age() - Expected 93. Got {other.get_age}")
-    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage}")
-    print(f"{other.name} is_vintage() - Expected False. Got {other.is_vintage}")
+    print(f"{gibson.name} get_age() - Expected 103. Got {gibson.get_age()}")
+    print(f"Other {other.name} get_age() - Expected 93. Got {other.get_age()}")
+    print(f"{gibson.name} is_vintage() - Expected True. Got {gibson.is_vintage()}")
+    print(f"{other.name} is_vintage() - Expected False. Got {other.is_vintage()}")
 
 run_test()
