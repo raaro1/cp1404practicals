905262ad88a4e3be5d177b16bab4217eaa70a5bd
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-02 19:24:15 +1000

    Add str method to return f string and add attributes in init method

---
+++ prac_06/programming_language.py
@@ -0,0 +1,18 @@
+"""
+Estimate: 15 minutes
+Actual:
+"""
+
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        pass
+
+
+    def __str__(self):
+        return f"{self.name} ({self.typing}, {self.reflection}, {self.year})"
