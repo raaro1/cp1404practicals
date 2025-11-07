48a88b9727c8986536b73a6bfd591fe889ee941a
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-08 02:52:55 +1000

    Add previous practical guitar input code

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,8 +1,22 @@
 from guitar import Guitar
 
-
 guitars = []
-# Open the file for reading
+
+print("My guitars!")
+out_file = open("guitars.csv", "a",newline="")
+name = input("Name: ")
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: "))
+    add_guitar = Guitar(name, year, cost)
+    guitars.append(add_guitar)
+    print(add_guitar, "added.")
+    name = input("Name: ")
+
+for guitar in guitars:
+    out_file.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
+out_file.close()
+
 in_file = open('guitars.csv', 'r')
 in_file.readline()
 for line in in_file:
@@ -10,9 +24,10 @@ for line in in_file:
     name = parts[0]
     year = parts[1]
     cost = parts[2]
-    guitar_attributes = Guitar(name, year, float(cost))
+    guitar_attributes = Guitar(name, int(year), float(cost))
     guitars.append(guitar_attributes)
 in_file.close()
+
 
 print("\nBefore Sorting\n")
 for guitar_attributes in guitars:
