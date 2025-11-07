275ba20dca3e18de7ad878f2861fc24c8d938903
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-08 01:45:48 +1000

    Add pointer arithmetic attribute to init function and repr function

--- prac_07/programming_language.py
+++ prac_07/programming_language.py
@@ -7,16 +7,21 @@ Programming Language class with tests.
 class ProgrammingLanguage:
     """Represent information about a programming language."""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name, typing, reflection, year, pointer_arithmetic):
         """Construct a ProgrammingLanguage from the given values."""
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
+        self.pointer_arithmetic = pointer_arithmetic
 
     def __repr__(self):
         """Return string representation of a ProgrammingLanguage."""
-        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}, pointer arithmetic={self.pointer_arithmetic}"
+
+    def is_pointer_arithmetic(self):
+        """Return True if this is a pointer arithmetic."""
+        return self.pointer_arithmetic == "Yes"
 
     def is_dynamic(self):
         """Determine if language is dynamically typed."""
@@ -25,9 +30,9 @@ class ProgrammingLanguage:
 
 def run_tests():
     """Run simple tests/demos on ProgrammingLanguage class."""
-    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
-    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
-    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995, "False")
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991,"False")
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991,"False")
 
     languages = [ruby, python, visual_basic]
     print(python)
