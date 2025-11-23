0bc3befc50672580fe00268a9f3fe3c6c483a2e6
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-23 20:18:29 +1000

    Create test using assert

---
+++ prac_09/silver_service_taxi_test.py
@@ -0,0 +1,12 @@
+from prac_09.silver_service_taxi import SilverServiceTaxi
+
+
+def main():
+    """Test using assert statements."""
+    taxi = SilverServiceTaxi("Hummer", 200, 2)
+    taxi.drive(18)
+    fare = taxi.get_fare()
+
+    assert abs(fare - 48.78) < 0.01, f"Expected 48.78, got {fare}"
+
+main()
