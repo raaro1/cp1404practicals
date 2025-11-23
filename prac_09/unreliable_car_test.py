9b63aa4706eb32f362b74fd665ac722cfd240348
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-23 19:28:36 +1000

    Create a test for the drive function based on car's reliability

--- prac_09/unreliable_car_test.py
+++ prac_09/unreliable_car_test.py
@@ -2,8 +2,11 @@ from unreliable_car import UnreliableCar
 
 def main():
     """Test to see if drive function works"""
-    test_car = UnreliableCar("Miata",100,30)
-    for i in range (1,100):
-        test_car.drive(i)
+    bad_car = UnreliableCar("Miata",100,30)
+    good_car = UnreliableCar("Toyota",100,80)
 
-    main()
+    for i in range (1,10):
+        print(f"{good_car.name} drove {good_car.drive(i)}")
+        print(f"{bad_car.name} drove {bad_car.drive(i)}")
+
+main()
