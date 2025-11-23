6205ff38d9c2b0918f572b8e99ec12f34bf7767f
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-23 18:06:09 +1000

    fix missing parameters for both car and taxi classes

--- prac_09/car.py
+++ prac_09/car.py
@@ -4,13 +4,14 @@
 class Car:
     """Represent a Car object."""
 
-    def __init__(self, fuel=0):
+    def __init__(self, name, fuel=0):
         """Initialise a Car instance.
 
         fuel: float, one unit of fuel drives one kilometre
         """
         self.fuel = fuel
         self._odometer = 0
+        self.name = name
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
--- prac_09/taxi.py
+++ prac_09/taxi.py
@@ -8,7 +8,7 @@ from prac_09.car import Car
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name,fuel, price_per_km):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
         self.price_per_km = price_per_km
