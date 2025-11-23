d6fd695983f01583ab5df79637f01f4e38363da0
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-24 01:46:44 +1000

    Remove incorrect formatting

--- prac_09/car.py
+++ prac_09/car.py
@@ -19,7 +19,7 @@ class Car:
 
     def __str__(self):
         """Return a string representation of a Car object."""
-        return f"{self.name}, fuel={self.fuel:.1f}, odometer={self.odometer:.1f}"
+        return f"{self.name}, fuel={int(self.fuel)}, odometer={int(self.odometer)}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
