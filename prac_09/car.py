49a33db8647d56e0b9710613416587687dc3b303
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-24 01:42:49 +1000

    Add formatting

--- prac_09/car.py
+++ prac_09/car.py
@@ -19,7 +19,7 @@ class Car:
 
     def __str__(self):
         """Return a string representation of a Car object."""
-        return f"{self.name}, fuel={self.fuel}, odometer={self.odometer}"
+        return f"{self.name}, fuel={self.fuel:.1f}, odometer={self.odometer:.1f}"
 
     def add_fuel(self, amount):
         """Add amount to the car's fuel."""
