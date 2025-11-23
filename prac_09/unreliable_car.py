749d6c05140a7e4e303b70067343353954a21066
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-23 19:08:28 +1000

    Add drive function that using randint to generate chance

---
+++ prac_09/unreliable_car.py
@@ -0,0 +1,22 @@
+from prac_09.car import Car
+from random import randint
+
+
+class UnreliableCar(Car):
+    """Extension of Car that considers the reliability of the car"""
+    def __init__(self,name, fuel,reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self,distance):
+        chance = randint(0,100)
+        if chance < self.reliability:
+            return super().drive(distance)
+        else:
+            return 0
+
+
+
+
+
+
