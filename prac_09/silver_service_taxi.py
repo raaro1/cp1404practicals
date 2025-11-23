6486fa3a971e5113ec3ac198428ae823690b10f8
raaro1 <roi.aaron@my.jcu.edu.au>
2025-11-23 20:25:38 +1000

    Remove spaces from docstring

--- prac_09/silver_service_taxi.py
+++ prac_09/silver_service_taxi.py
@@ -5,7 +5,7 @@ class SilverServiceTaxi(Taxi):
     flag_fall = 4.50
 
     def __init__(self, name, fuel, fanciness):
-        """ Initialise a Taxi instance, based on parent class Car. """
+        """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name,fuel)
         self.fanciness = fanciness
         self.price_per_km = Taxi.price_per_km
